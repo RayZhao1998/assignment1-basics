a48535de9ae1c71d29797d5a18b1e856f01fb09e
Ziyuan Zhao <ninjiacoder@gmail.com>
2025-11-13 16:09:55 +0800

    feat: support Linear module

---
+++ cs336_basics/linear.py
@@ -0,0 +1,13 @@
+import torch
+
+class Linear(torch.nn.Module):
+    def __init__(self, in_features, out_features, device=None, dtype=None):
+        super().__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weights = torch.nn.Parameter(torch.empty(out_features, in_features, device=device, dtype=dtype))
+        self.device = device
+        self.dtype = dtype
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return x @ self.weights.T
